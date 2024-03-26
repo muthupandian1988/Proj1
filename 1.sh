0e90572cf4b3fbb3d8b8cf2f9852feb5f56a449f
muthupandian8 <muthupandian8@gmail.com>
2024-03-26 21:35:23 +0530

    For QA environment

---
+++ 1.sh
@@ -0,0 +1,43 @@
+#!/bin/bash   
+branch=$1
+env="dev"
+shopt -s nocasematch
+if [ "$2" == "master" ];
+then
+	case $1 in 'env')
+	echo 'Production'
+	;;
+	
+	'appservice')
+	echo 'Prod-DevopsLearn'
+	;;
+	*)
+    echo -n "Invalid"
+    ;;
+	esac
+elif [ "$2" == "QA" ];
+then
+	case $1 in 'env')
+	echo 'QA'
+	;;
+	
+	'appservice')
+	echo 'QA-DevopsLearn'
+	;;
+	*)
+    echo -n "Invalid"
+    ;;
+	esac
+else
+	case $1 in 'env')
+	echo 'dev'
+	;;
+	
+	'appservice')
+	echo 'Dev-DevOpsLearn'
+	;;
+	*)
+    echo -n "Invalid"
+    ;;
+	esac
+fi
